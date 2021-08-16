a71a67694623b7d7f5ca9640ff57c6d0de8df35f
Gabriel Duarte <gabrield.developer@gmail.com>
2021-08-15 23:24:48 -0300

    feat(inferencia): testing javas type inference

---
+++ src/fundamentos/Inferencia.java
@@ -0,0 +1,24 @@
+package fundamentos;
+
+public class Inferencia {
+	public static void main(String[] args) {
+		double a = 4.5;
+		a = 12.456;
+		var b = 4.5;
+		b = 10;
+		
+		var c = "texto";
+		
+		c = "outro texto";
+		
+		// nao pode
+		// c = 4.5;
+		
+		double d; // declarada
+		d = 123.65; // inicializada
+		
+		// não pode fazer separado com o var
+//		var e;
+//		e = 12354.332;
+	}
+}
