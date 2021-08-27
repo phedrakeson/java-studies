0c8ca63b143f78b18011805813fdef19c4ebbd58
Gabriel Duarte <gabrield.developer@gmail.com>
2021-08-26 23:35:18 -0300

    feat: unary operators examples

---
+++ src/fundamentos/operadores/Unarios.java
@@ -0,0 +1,15 @@
+package fundamentos.operadores;
+
+public class Unarios {
+
+	public static void main(String[] args) {
+		
+		int a = 1;
+		int b = 1;
+		
+		a++; // a = a + 1
+		b--; // b = b - 1
+		
+	}
+
+}
