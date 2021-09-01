c3c81290cd5f7e1ab6ecf04673b4fa4ce1230ff7
Gabriel Duarte <gabrield.developer@gmail.com>
2021-08-31 23:49:20 -0300

    feat: for examples

---
+++ src/controle/For.java
@@ -0,0 +1,21 @@
+package controle;
+
+public class For {
+
+	public static void main(String[] args) {
+		
+		for(int contador = 0; contador <= 20; contador+= 2) {
+			System.out.printf("i = %d\n", contador);
+		}
+		
+		int x = 0;
+		for(; x < 10; x++) {
+			System.out.println("x = " + x);
+		}
+		
+		for(;;) {
+			System.out.println("laço infinito");
+		}
+	}
+
+}
