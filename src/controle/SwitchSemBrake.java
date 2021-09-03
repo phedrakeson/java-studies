dca7cddd668fc46c0cb6c6768f82dadcf2f7921a
Gabriel Duarte <gabrield.developer@gmail.com>
2021-09-03 00:04:56 -0300

    feat: switch case example without break

---
+++ src/controle/SwitchSemBrake.java
@@ -0,0 +1,27 @@
+package controle;
+
+public class SwitchSemBrake {
+
+	public static void main(String[] args) {
+		
+		String faixa = "preta";
+		
+		switch (faixa.toLowerCase()) 
+		{
+			case "preta":
+				System.out.println("Sei o Bassai-Dai...");
+			case "marrom":
+				System.out.println("Sei o Tekki Shodan...");
+			case "roxa":
+				System.out.println("Sei o Heian Godan...");
+			case "verde":
+				System.out.println("Sei o Heian Yodan...");
+			case "laranja":
+				System.out.println("Sei o Heian Sandan...");
+			default:
+				System.out.println("Não sei de nada...");
+		}
+		)
+	}
+
+}
