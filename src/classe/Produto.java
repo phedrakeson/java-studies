9e0614bf49b1629f9b8aa35527f1deb04f033ce0
Gabriel Duarte <gabrield.developer@gmail.com>
2021-09-09 00:09:25 -0300

    feat: class examples

--- src/classe/Produto.java
+++ src/classe/Produto.java
@@ -1,9 +1,12 @@
 package classe;
 
 public class Produto {
-
+	String nome;
+	double preco;
+	double desconto;
+	
 	public static void main(String[] args) {
-		// TODO Auto-generated method stub
+		
 
 	}
 
--- src/classe/ProdutoTeste.java
+++ src/classe/ProdutoTeste.java
@@ -3,7 +3,19 @@ package classe;
 public class ProdutoTeste {
 
 	public static void main(String[] args) {
-		// TODO Auto-generated method stub
+		
+		Produto p1 = new Produto();
+		p1.nome = "Paçoca";
+		p1.preco = 14.32;
+		p1.desconto = 0.25;
+		
+		
+		var p2 = new Produto();
+		p2.preco = 12.56;
+		p2.desconto = 0.29;
+		p2.nome = "Leite Fresco";
+		
+		System.out.println(p1.nome + " " + p2.nome);
 
 	}
 
