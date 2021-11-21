fe0de474445e06313a4a48eaa928d3063dcad220
Gabriel Duarte <gabrield.developer@gmail.com>
2021-11-20 22:42:20 -0300

    feat(heranca): implements enum examples

---
+++ src/oo/heranca/Jogo.java
@@ -0,0 +1,14 @@
+package oo.heranca;
+
+public class Jogo {
+
+	public static void main(String[] args) {
+		
+		Jogador j1 = new Jogador();
+		
+		j1.andar(Direcao.LESTE);
+		j1.andar(Direcao.OESTE);
+
+	}
+
+}
