3371d78edc63d9bf8ff64df7227aa304d8dc4ac7
Gabriel Duarte <gabrield.developer@gmail.com>
2021-11-20 22:52:29 -0300

    feat(heranca): creates monster and hero classes

--- src/oo/heranca/Jogo.java
+++ src/oo/heranca/Jogo.java
@@ -4,10 +4,20 @@ public class Jogo {
 
 	public static void main(String[] args) {
 		
-		Jogador j1 = new Jogador();
+		Monstro j1 = new Monstro();
+		j1.x = 10;
+		j1.y = 10;
+		Heroi j2 = new Heroi();
+		j2.x = 10;
+		j2.y = 11;
+		
 		
 		j1.andar(Direcao.LESTE);
 		j1.andar(Direcao.OESTE);
+		
+		j1.atacar(j2);
+		
+		System.out.println(j2.vida);
 
 	}
 
